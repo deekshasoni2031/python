c4d6edc427889f713f3ed938d658d2bd3275f7d8
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-04-27 22:24:05 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,18 @@
+a=1  #integer
+b=True #boolean
+c="deeksha"  #string
+d=None
+print(type(a))
+print(type(d))
+print(type(c))
+print(type(b))
+
+#sequenced data:list,tuple
+list1=[5,7,8.9,12,[2,3],["apple","banana"]]
+print(list1)
+tuple1=(("parrot","sparrow"),("lion","tiger"))
+print(tuple1)
+
+#mapped data:dictionary
+dict1={"name":"deeksha","age":19,"canvote":"true"}
+print(dict1)
