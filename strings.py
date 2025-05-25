019233fef1960ec2043d5c81fae5a2e0cbe1eb25
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-05-25 11:07:55 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,7 @@
+#strings in python
+name="deeksha" #string can be enclosed in both single and double quotes
+print("hello",name)
+
+#supoose we have to print a string in double quotes then we can write it under single quotes
+print('he said,"I want to eat an apple."')
+#triple quotes are also used to print a paragraph
