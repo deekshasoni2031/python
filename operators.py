d63e88b8cb51bc975e46ec990bfdf39b000cd916
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-05-25 11:06:12 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,8 @@
+#all types of operators used in python
+print(15+6) #addition
+print(15-6) #subtraction
+print(15*6) #multiplication
+print(15/6) #division
+print(15//6) #floor division
+print(15**6) #power op.
+print(15%6) #modulus(return remainder)
