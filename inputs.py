6ed84eb9b2cb60d7b0b3fa1e9eeaa2e0cd856a09
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-05-25 11:07:36 +0530

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,10 @@
+#taking inputs in python
+a=str(input("enter your name: "))
+b=str(input("enter your father's name: "))
+c=str(input("enter your mother's name: "))
+d=int(input("enter you D.O.B: "))
+
+x=input("enter first number: ")
+y=input("enter second number: ")
+print(x+y) #it will take input as a string and concatenate them
+print(int(x)+int(y)) #via tyopecasting it will give output in integer format
