d074e7b87ae77a7c491224ee1fc3e1f627157e49
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-05-27 11:17:47 +0530

    Add files via upload

--- strings.py
+++ strings.py
@@ -4,4 +4,8 @@ print("hello",name)
 
 #supoose we have to print a string in double quotes then we can write it under single quotes
 print('he said,"I want to eat an apple."')
-#triple quotes are also used to print a paragraph
+#triple quotes are also used to print a paragraph1
+
+#accessing characters of a string
+print(name[3])  #[3]=index no. 3
+print(name[5])
