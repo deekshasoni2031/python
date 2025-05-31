362d2bf9c7934a6dce24f6693e12afccc77d1743
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-05-31 10:57:13 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,23 @@
+#example 1
+count =5
+while (count >0):
+    print(count)
+    count=count-1
+
+#example 2
+for i in range(3):
+    print(i)
+i=0
+while(i<3):
+    print(i)
+    i=i+1
+    print("done with the loop")
+
+#example 3
+i=0
+while True:
+    print(i)
+    i=i+1
+    if(i%100==0):
+        break
+
