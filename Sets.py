37a6e2e9d1ac7307d2b98a7739842f5c081fc164
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-06-08 22:49:06 +0530

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,20 @@
+# sets in python
+s={1,2,3,4,5}
+print(s)
+
+info={"rahul",18,False,"eligible"}
+print(info)
+
+for value in info:
+    print(value)
+
+# by this statement an empty dictionary will be created
+deeksha={}
+print(type(deeksha))
+
+# by this an empty set is created
+#deeksha1= set{}
+#print(type(deeksha1))
+
+
+
