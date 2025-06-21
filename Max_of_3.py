69bdb5cb0f9c78db55e27380b253cb272d1970b6
deekshasoni2031 <deekshasoni2031@gmail.com>
2025-06-21 11:13:29 +0530

    Add files via upload

---
+++ Max_of_3.py
@@ -0,0 +1,12 @@
+# printing maximum of 3 numbers
+
+a= input("enter the value of a: ")
+b= input("enter the value of b: " )
+c= input("enter the value of c: ")
+if a>b and a>c:
+    print("a is greater number",a)
+elif b>a and b>c:
+    print("b is a greater number",b)
+else:
+    print("c is a greater number",c)
+   
